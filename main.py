818f1f81c44957c6fd875819d30007c2dd29aa05
Pratham Gupta <36500499+itspratham@users.noreply.github.com>
2023-01-03 18:44:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 from datetime import datetime
 
 from fastapi import FastAPI
@@ -14,13 +15,20 @@ class Item(BaseModel):
     destination_station_id: str
     date: str
 
+class Item1(BaseModel):
+    origin_station_id: str
+    destination_station_id: str
+    date: str
 
-@app.get("/")
+@app.get("/api/light-rail/")
 async def root(item: Item):
     try:
         item = item.dict()
+        try:
+            datetime.strptime(item["date"],"%Y-%m-%d")
+        except:
+            return {"message": "Invalid Date Format."}
         datee = item["date"].split("-")
-        print(datee)
         year = datee[0]
         month = datee[1]
         date_ = datee[2]
@@ -28,8 +36,11 @@ async def root(item: Item):
         destination_station_id = item["destination_station_id"]
         url = "https://traintime.lirr.org/api/TrainTime?month={}&day={}&year={}&datoggle=d&endsta={}&startsta={}&&api_key=dwrxfpcsp42pcwxfbpd90zde5fjeb05pcdaqjw3j".format(month, date_, year, destination_station_id, origin_station_id)
         x = requests.get(url)
+        # print(x.json())
         item_list = x.json()
         a_list = []
+        if len(item_list["TRIPS"]) == 0:
+            return {"message": "0 trip found"}
         for i in range(len(item_list["TRIPS"])):
             dt = parser.parse(item["date"])
             start_ = datetime.strptime(item_list["TRIPS"][i]["LEGS"][0]["DEPART_TIME"], '%H%M').time()
@@ -37,6 +48,42 @@ async def root(item: Item):
             start_combined = datetime.strftime(dt.combine(dt, start_), "%m-%d-%Y %H:%M:%S")
             end_combined = datetime.strftime(dt.combine(dt, end_), "%m-%d-%Y %H:%M:%S")
             a_list.append({"eta_origin": start_combined, "eta_destination": end_combined})
-        return [{"transit mode": "rail"}, a_list]
+        return {"transit mode": "light_rail", "time_slots": a_list}
     except:
-        return []
+        return {"message": "There is some error occurred while requesting"}
+
+from dateutil import parser
+
+@app.get("/api/rail/")
+async def root1(item: Item):
+    try:
+        item = item.dict()
+        try:
+            datetime.strptime(item["date"],"%Y-%m-%d")
+        except:
+            return {"message": "Invalid Date Format."}
+        datee = item["date"].split("-")
+        year = datee[0]
+        month = datee[1]
+        date_ = datee[2]
+        origin_station_id = item["origin_station_id"]
+        destination_station_id = item["destination_station_id"]
+        # url = "https://traintime.lirr.org/api/TrainTime?month={}&day={}&year={}&datoggle=d&endsta={}&startsta={}&&api_key=dwrxfpcsp42pcwxfbpd90zde5fjeb05pcdaqjw3j".format(month, date_, year, destination_station_id, origin_station_id)
+        url = "https://mnorth.prod.acquia-sites.com/wse/MYmta/Trains/v4/{}/{}/DepartBy/{}/{}/{}/0000/9ea83a8a361efacd098b6c7f6a6e49c1/Tripstatus24".format(origin_station_id, destination_station_id, str(year), str(int(month)),
+                                                                            str(int(date_)) )
+        print(url)
+        x = requests.get(url)
+        # print(x.json())
+        item_list = x.json()
+        a_list = []
+        if len(item_list["GetTripStatusJsonResult"]) == 0:
+            return {"message": "0 trip found"}
+        for i in range(len(item_list["GetTripStatusJsonResult"])):
+            start_ = parser.isoparse(item_list["GetTripStatusJsonResult"][i]["OriginDateTime"])
+            start_ = datetime.strftime(start_, "%m-%d-%Y %H:%M:%S")
+            end_ = parser.isoparse(item_list["GetTripStatusJsonResult"][i]["DestinationDateTime"])
+            end_ = datetime.strftime(end_, "%m-%d-%Y %H:%M:%S")
+            a_list.append({"eta_origin": start_, "eta_destination": end_})
+        return {"transit mode": "rail", "time_slots": a_list}
+    except:
+        return {"message": "There is some error occurred while requesting"}
